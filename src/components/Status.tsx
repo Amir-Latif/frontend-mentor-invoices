c0c60c47ce0149415a1e383cca68018b73c7e59d
Amir Latif <amirwesa@gmail.com>
2021-11-18 03:17:15 +0200

    control buttons colod and added draft styling

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -2,7 +2,7 @@ import './status.css'
 
 export default function Status({ status, theme, className }: { status: string, theme: string, className?: string }) {
     return (
-        <div className={`${status === 'Paid' ? "paid" : "pending"} ${theme === 'night' ? 'night-status' : ''} status d-flex align-items-center fw-bold ${className}`}>
+        <div className={`${status.toLowerCase()} ${theme === 'night' ? 'night-status' : ''} status d-flex align-items-center fw-bold ${className}`}>
             <div className="bullet rounded-circle"></div>
             <div>{status}</div>
         </div>
